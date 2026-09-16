1b7c0a0c7b7f9aba045bb899659182e23443adfd
Primerano60 <martin.primerano@outlook.com>
2026-09-16 14:22:38 +0200

    ändring 14:23

--- src/main/java/se/iths/sebastian/libaryapp/Main.java
+++ src/main/java/se/iths/sebastian/libaryapp/Main.java
@@ -6,20 +6,53 @@ import se.iths.sebastian.libaryapp.model.Borrower;
 import se.iths.sebastian.libaryapp.model.Loan;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static void main() {
-        Author author = new Author("HP.Lovecraft");
-        Book book = new Book("Call of Cuthulu", 123, author);
-        IO.println(book);
 
-        Borrower borrower = new Borrower("gamer 23", "Lösrenord12345!", "Kevin");
+        Author author1 = new Author("Margareta");
+        Author author2 = new Author("LIlly");
 
-        IO.println(borrower);
+        List<Borrower> borrowers = new ArrayList<>();
+        borrowers.add(new Borrower("Gamer 23", "Lösenord12345!", "Kalle"));
+        borrowers.add(new Borrower("Fizen", "grabben123", "Sebbe"));
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Hopp", 56745855, author1));
+        books.add(new Book("Tro", 48655855, author1));
+        books.add(new Book("Frihet", 78955655, author2));
+        List<Loan> loans = new ArrayList<>();
 
-        Loan loan = new Loan(LocalDateTime.now(), borrower, book);
-        IO.println(loan);
+        boolean appRunner = true;
+        while (appRunner){
 
+            Borrower currentBorrower = login(borrowers );
+            if (currentBorrower == null ){
+
+                appRunner = false;
+            }
+        }
 
     }
+
+
+
+        public static Borrower login (List<Borrower> borrowers) {
+
+        IO.println("Skriv in inlogg");
+
+        String username = IO.readln("Username: ");
+        String password = IO.readln("Password: ");
+        Borrower currentBorrower = null;
+
+        for(Borrower borrower:borrowers){
+
+            if (borrower.getUsername().equals(username) && borrower.getPassword().equals(password));
+            currentBorrower = borrower;
+        }
+
+
+            return currentBorrower;
+        }
 }
