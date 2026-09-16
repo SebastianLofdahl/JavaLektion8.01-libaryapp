5c6315b43c6c459a19c3887b6b07beaa7427934e
SebastianLofdahl <randomgametales@gmail.com>
2026-09-16 14:59:03 +0200

    Ändring 14:58 16/9/26

--- src/main/java/se/iths/sebastian/libaryapp/Main.java
+++ src/main/java/se/iths/sebastian/libaryapp/Main.java
@@ -5,7 +5,6 @@ import se.iths.sebastian.libaryapp.model.Book;
 import se.iths.sebastian.libaryapp.model.Borrower;
 import se.iths.sebastian.libaryapp.model.Loan;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,21 +23,62 @@ public class Main {
         books.add(new Book("Frihet", 78955655, author2));
         List<Loan> loans = new ArrayList<>();
 
-        boolean appRunner = true;
-        while (appRunner){
+        boolean appIsRunning = true;
+        while (appIsRunning) {
 
-            Borrower currentBorrower = login(borrowers );
-            if (currentBorrower == null ){
+            Borrower currentBorrower = login(borrowers);
+            if (currentBorrower == null) {
 
-                appRunner = false;
+                appIsRunning = false;
             }
+
+            boolean loggedIn = true;
+
+            while (loggedIn) {
+                IO.println("-- Meny --");
+                IO.println("1. Lista av alla böcker");
+                IO.println("2. Låna bok");
+                IO.println("3. Avsluta");
+
+                String choice = IO.readln("Välj mellan 1-2: ");
+
+                switch (choice) {
+                    case "1" -> listBooks(books);
+                    case "2" -> borrowBook(books);
+                    case "3" -> {
+                        loggedIn = false;
+                        appIsRunning = false;
+                    }
+                    default -> IO.println("Error! Väl endast 1 eller 2.");
+                }
+            }
+
+        }
+
+    }
+
+    private static void listBooks(List<Book> books) {
+        for (Book book : books) {
+            IO.println(book);
+        }
+    }
+
+    private static void borrowBook(List<Book> books) {
+        String isbn = IO.readln("ISBN: ");
+
+        Book foundBook = null;
+        for (Book book : books) {
+            foundBook = book;
+            break;
+        }
+        if (foundBook == null) {
+            IO.println("Ingen bok med det nummret hittades");
         }
 
     }
 
 
-
-        public static Borrower login (List<Borrower> borrowers) {
+    public static Borrower login(List<Borrower> borrowers) {
 
         IO.println("Skriv in inlogg");
 
@@ -46,13 +86,18 @@ public class Main {
         String password = IO.readln("Password: ");
         Borrower currentBorrower = null;
 
-        for(Borrower borrower:borrowers){
+        for (Borrower borrower : borrowers) {
 
-            if (borrower.getUsername().equals(username) && borrower.getPassword().equals(password));
+            if (borrower.getUsername().equals(username) && borrower.getPassword().equals(password)) ;
             currentBorrower = borrower;
         }
-
-
-            return currentBorrower;
+        if (currentBorrower != null) {
+            System.out.println("Inloggad som " + currentBorrower);
+        } else {
+            System.out.println("Fel username eller password.");
         }
+
+
+        return currentBorrower;
+    }
 }
