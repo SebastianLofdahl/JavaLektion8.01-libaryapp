a0d165c1414c7a0a3ec9b3261594a94c8c8d86bf
SebastianLofdahl <randomgametales@gmail.com>
2026-09-16 13:30:39 +0200

    Ändring 13:30 16/9/26

--- src/main/java/se/iths/sebastian/libaryapp/Main.java
+++ src/main/java/se/iths/sebastian/libaryapp/Main.java
@@ -3,6 +3,9 @@ package se.iths.sebastian.libaryapp;
 import se.iths.sebastian.libaryapp.model.Author;
 import se.iths.sebastian.libaryapp.model.Book;
 import se.iths.sebastian.libaryapp.model.Borrower;
+import se.iths.sebastian.libaryapp.model.Loan;
+
+import java.time.LocalDateTime;
 
 public class Main {
     static void main() {
@@ -10,9 +13,13 @@ public class Main {
         Book book = new Book("Call of Cuthulu", 123, author);
         IO.println(book);
 
-        Borrower borrower = new Borrower("gamer 23", "Lösrenord12345!","Kevin");
+        Borrower borrower = new Borrower("gamer 23", "Lösrenord12345!", "Kevin");
 
         IO.println(borrower);
+
+        Loan loan = new Loan(LocalDateTime.now(), borrower, book);
+        IO.println(loan);
+
 
     }
 }
--- src/main/java/se/iths/sebastian/libaryapp/model/Loan.java
+++ src/main/java/se/iths/sebastian/libaryapp/model/Loan.java
@@ -1,4 +1,33 @@
 package se.iths.sebastian.libaryapp.model;
 
+import java.time.LocalDateTime;
+
 public class Loan {
+
+    private LocalDateTime loanDate;
+    private Borrower borrower;
+    private Book book;
+
+    public Loan(LocalDateTime loanDate, Borrower borrower, Book book) {
+        this.loanDate = LocalDateTime.now();
+        this.borrower = borrower;
+        this.book = book;
+    }
+
+    public LocalDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    @Override
+    public String toString() {
+        return borrower.getName() + " lånade: " + book.getTitle() + " den: " + loanDate;
+    }
 }
